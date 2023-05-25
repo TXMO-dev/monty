41b9427468f52636a12451b7ae304d39385e2e0b
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-05-25 15:56:40 -0700

    worked on swap

--- opcode_functions.c
+++ opcode_functions.c
@@ -92,6 +92,9 @@ pint(stack, line_number);
 } else if (strcmp(opcode, "pop") == 0)
 {
 pop(stack, line_number);
+} else if(strcmp(opcode, "swap") == 0)
+{
+swap(stack, line_number);
 }
 }
 
@@ -115,6 +118,25 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (*stack != NULL)
 		(*stack)->prev = NULL;
 	free(temp);
+}
+/**
+ * swap - Swaps the top two elements of the stack
+ * @stack: Double pointer to the stack
+ * @line_number: Line number in the file
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
 }
 /**
  * execute - Pushes an element to the stack
