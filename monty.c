5e718e9be34441d5f806faf0cd4f99d1eeb7dfce
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-05-25 15:35:13 -0700

    refined opcodes

--- monty.c
+++ monty.c
@@ -4,36 +4,36 @@
 #include "monty.h"
 
 #define BUFFER_SIZE 1024
-
+/**
+ * main - Entry point for the Monty bytecode interpreter
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 on success, EXIT_FAILURE on failure
+ */
 int main(int argc, char *argv[])
 {
-    FILE *file;
-    stack_t *stack = NULL;
-    char buffer[BUFFER_SIZE];
-    unsigned int line_number = 0;
-
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-
-    while (fgets(buffer, BUFFER_SIZE, file) != NULL)
-    {
-        line_number++;
-        execute_opcode(&stack, buffer, line_number);
-    }
-
-    free_stack(stack);
-    fclose(file);
-
-    return 0;
+FILE *file;
+stack_t *stack = NULL;
+char buffer[BUFFER_SIZE];
+unsigned int line_number = 0;
+if (argc != 2)
+{
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
 }
-
+file = fopen(argv[1], "r");
+if (file == NULL)
+{
+fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+exit(EXIT_FAILURE);
+}
+while (fgets(buffer, BUFFER_SIZE, file) != NULL)
+{
+line_number++;
+execute_opcode(&stack, buffer, line_number);
+}
+free_stack(stack);
+fclose(file);
+return (0);
+}
--- opcode_functions.c
+++ opcode_functions.c
@@ -3,102 +3,130 @@
 #include <string.h>
 #include <ctype.h>
 #include "monty.h"
-
+/**
+ * push - Pushes an element to the stack
+ * @stack: Double pointer to the stack
+ * @line_number: Line number in the file
+ */
 void push(stack_t **stack, unsigned int line_number)
 {
-    char *value_str;
-    int value;
-
-    value_str = strtok(NULL, " \t\n");
-    if (value_str == NULL || !is_integer(value_str))
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    value = atoi(value_str);
-    push_stack(stack, value);
+char *value_str;
+int value;
+value_str = strtok(NULL, " \t\n");
+if (value_str == NULL || !is_integer(value_str))
+{
+fprintf(stderr, "L%u: usage: push integer\n", line_number);
+exit(EXIT_FAILURE);
+}
+value = atoi(value_str);
+push_stack(stack, value);
 }
 
+/**
+ * pall - Pushes an element to the stack
+ * @stack: Double pointer to the stack
+ * @line_number: Line number in the file
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    (void)line_number;
-
-    print_stack(*stack);
+(void)line_number;
+print_stack(*stack);
 }
 
+/**
+ * pus_stack - Pushes an element to the stack
+ * @stack: Double pointer to the stack
+ * @value: Line number in the file
+ */
 void push_stack(stack_t **stack, int value)
 {
-    stack_t *new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    new_node->n = value;
-    new_node->next = *stack;
-    *stack = new_node;
+stack_t *new_node = malloc(sizeof(stack_t));
+if (new_node == NULL)
+{
+fprintf(stderr, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
+}
+new_node->n = value;
+new_node->next = *stack;
+*stack = new_node;
 }
 
+/**
+ * print - Pushes an element to the stack
+ * @stack: Double pointer to the stack
+ * @line_number: Line number in the file
+ */
 void print_stack(stack_t *stack)
 {
-    while (stack != NULL)
-    {
-        printf("%d\n", stack->n);
-        stack = stack->next;
-    }
+while (stack != NULL)
+{
+printf("%d\n", stack->n);
+stack = stack->next;
 }
-
+}
+/**
+ * execute - Pushes an element to the stack
+ * @stack: Double pointer to the stack
+ * @line_number: Line number in the file
+ */
 void execute_opcode(stack_t **stack, char *opcode, unsigned int line_number)
 {
-    if (strcmp(opcode, "push") == 0)
-    {
-        char *arg = strtok(NULL, " \t\n");
-        int value;
-        if (arg == NULL || !is_integer(arg))
-        {
-            fprintf(stderr, "L%d: usage: push integer\n", line_number);
-            exit(EXIT_FAILURE);
-        }
-
-        value = atoi(arg);
-        push_stack(stack, value);
-    }
-    else if (strcmp(opcode, "pall") == 0)
-    {
-        print_stack(*stack);
-    }
+if (strcmp(opcode, "push") == 0)
+{
+char *arg = strtok(NULL, " \t\n");
+int value;
+if (arg == NULL || !is_integer(arg))
+{
+fprintf(stderr, "L%d: usage: push integer\n", line_number);
+exit(EXIT_FAILURE);
 }
-
+value = atoi(arg);
+push_stack(stack, value);
+}
+else if (strcmp(opcode, "pall") == 0)
+{
+print_stack(*stack);
+}
+}
+/**
+ * execute - Pushes an element to the stack
+ * @stack: Double pointer to the stack
+ * @line_number: Line number in the file
+ */
 int is_integer(const char *str)
 {
-    if (*str == '-')
-        str++;
-
-    if (*str == '\0')
-        return 0;
-
-    while (*str != '\0')
-    {
-        if (!isdigit(*str))
-            return 0;
-
-        str++;
-    }
-
-    return 1;
+if (*str == '-')
+{
+str++;
+}
+if (*str == '\0')
+{
+return (0);
+}
+while (*str != '\0')
+{
+if (!isdigit(*str))
+{
+return (0);
+}
+str++;
+}
+return (1);
 }
 
-
+/**
+ * execute - Pushes an element to the stack
+ * @stack: Double pointer to the stack
+ * @line_number: Line number in the file
+ */
 void free_stack(stack_t *stack)
 {
-    stack_t *current = stack;
-    while (current != NULL)
-    {
-        stack_t *temp = current;
-        current = current->next;
-        free(temp);
-    }
+stack_t *current = stack;
+while (current != NULL)
+{
+stack_t *temp = current;
+current = current->next;
+free(temp);
+}
 }
 
