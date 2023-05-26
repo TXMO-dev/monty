e9cd421e162755af59e481e923527f5a237a539e
Timothy Asigbey <asigbeytimothy@gmail.com>
2023-05-26 01:16:04 -0700

    added sub

--- opcode_functions.c
+++ opcode_functions.c
@@ -101,7 +101,26 @@ add(stack, line_number);
 } else if(strcmp(opcode, "nop") == 0)
 {
 nop(stack, line_number);
+} else if(strcmp(opcode, "sub" == 0)
+{
+sub(stack, line_number);
 }
+}
+/**
+ * sub - Subtracts the top element of the stack from the second top element
+ * @stack: Double pointer to the stack
+ * @line_number: Line number in the file
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n -= (*stack)->n;
+	pop_stack(stack, line_number);
 }
 
 /**
